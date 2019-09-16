67357455b291a65dcbba3debcde36dd93c1b9258
Christian Seki <chrisyuri_19@hotmail.com>
2019-09-16 02:27:18 -0300

    terminado : pontos e barras

--- sketch.js
+++ sketch.js
@@ -1,18 +1,45 @@
 ///<reference path="C:\Users\Seki\node_modules\@types\p5\global.d.ts"/>
+const nomes = {
+  0: "A",
+  1: "B",
+  2: "C",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "G",
+  7: "H",
+  8: "I",
+  9: "J",
+  10: "K",
+  11: "L",
+  12: "M",
+  13: "N",
+  14: "O",
+  15: "P",
+  16: "Q",
+  17: "R",
+  18: "S",
+  19: "T",
+  20: "U",
+  21: "V"
+};
+
+let barras = [];
+let componentesX = [];
+let componentesY = [];
+let pontos = [];
 
 let start_x = 0,
   start_y = 0,
   end_x = 0,
   end_y = 0;
 
-let flag_inicial = false,
-  flag_iniciou = false;
+let flag_inicio = false;
 
 function setup() {
-  let cnv = createCanvas(1000, 600);
+  createCanvas(1000, 600);
 
   strokeWeight(4);
-
   background("#f6f8fa");
   // put setup code here
 }
@@ -22,25 +49,102 @@ function draw() {
 }
 
 function mouseClicked() {
-  if (!flag_inicial) {
+  console.log(`ponto clicado: (${mouseX} , ${mouseY})`);
+
+  if (!flag_inicio) {
     start_x = mouseX;
     start_y = mouseY;
-    flag_inicial = true;
+
+    flag_inicio = true;
   } else {
     end_x = mouseX;
     end_y = mouseY;
-    line(start_x, start_y, end_x, end_y);
-    flag_iniciou = true;
+
+    //pontos.push(new Ponto(end_x, end_y, nomes[pontos.length]));
+    desenharPonto(start_x, start_y, nomes[pontos.length]);
+    desenharBarra(start_x, start_y, end_x, end_y);
+    //barras.push(new Barra(start_x, start_y, end_x, end_y));
+
     start_x = end_x;
     start_y = end_y;
   }
-  if (flag_iniciou) {
-    end_x = mouseX;
-    end_y = mouseY;
+}
 
-    line(start_x, start_y, end_x, end_y);
+class Vetor {
+  constructor(x, y) {
+    this.vector = createVector(x, y);
+  }
+}
+class Ponto {
+  constructor(x, y, name) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.DesenharPonto(x, y, name);
+  }
+  DesenharPonto(x, y, name) {
+    circle(x, y, 7);
 
-    start_x = end_x;
-    start_y = end_y;
+    textSize(20);
+    textLeading(30);
+    textStyle(BOLDITALIC);
+    text(name, x, y, 500, 500);
+  }
+}
+class Barra {
+  constructor(sx, sy, ex, ey) {
+    this.initx = sx;
+    this.inity = sy;
+    this.endx = ex;
+    this.endy = ey;
+    this.comprimento = this.DesenharBarra(sx, sy, ex, ey);
+  }
+  DesenharBarra(initx, inity, endx, endy) {
+    line(initx, inity, endx, endy);
+    const x = endx - initx;
+    const y = endy - inity;
+    const raiz = sqrt(x * x + y * y);
+    const comprimento = raiz.toPrecision(5);
+    return comprimento;
+  }
+}
+
+function desenharPonto(x, y, nome) {
+  if (barras.length !== 0) {
+    for (let i = 0; i < barras.length; i++) {
+      if (barras[i].endx === x && barras[i].endy === y) {
+        pontos.push(new Ponto(x, y, nome));
+        break;
+      }
+    }
+  }
+}
+
+function desenharBarra(startx, starty, endx, endy) {
+  let flag_desenhou = false;
+  if (barras.length === 0) barras.push(new Barra(startx, starty, endx, endy));
+  else {
+    let difx = 0;
+    let dify = 0;
+    for (let i = 0; i < barras.length; i++) {
+      endx > barras[i].initx
+        ? (difx = endx - barras[i].initx)
+        : (difx = barras[i].initx - endx);
+      endy > barras[i].inity
+        ? (dify = endy - barras[i].inity)
+        : (dify = barras[i].inity - endy);
+
+      console.log(`difx = ${difx} , dify = ${dify}`);
+
+      if (difx < 100 && dify < 100) {
+        barras.push(
+          new Barra(startx, starty, barras[i].initx, barras[i].inity)
+        );
+        end_x = barras[i].initx;
+        end_y = barras[i].inity;
+        flag_desenhou = true;
+      }
+    }
+    if (!flag_desenhou) barras.push(new Barra(startx, starty, endx, endy));
   }
 }
